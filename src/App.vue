476506d7bf7394e1381927967e63252b7296e1b5
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2022-11-04 19:47:01 +0600

    route to home

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+
+useRouter().push({ name: 'home' });
 </script>
 
 <template>
-   <v-app class="lalala">
+   <v-app>
       <header>
-         <v-system-bar v-if="$vuetify.display.xs">
-            <v-icon icon="mdi-wifi-strength-4"></v-icon>
-            <v-icon icon="mdi-signal" class="ml-2"></v-icon>
-            <v-icon icon="mdi-battery" class="ml-2"></v-icon>
-            <span class="ml-2">{{ `${new Date().getHours()}:${new Date().getMinutes()}` }}</span>
-         </v-system-bar>
-
-         <div class="wrapper">
-            <nav>
-               <RouterLink to="/">Home</RouterLink>
-               <RouterLink to="/about">About</RouterLink>
-            </nav>
-         </div>
+         <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+         </nav>
       </header>
       <v-main>
          <RouterView></RouterView>
