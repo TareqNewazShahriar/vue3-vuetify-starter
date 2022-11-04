208368b8801e5b308b7d8eb159801666594ace23
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2022-11-04 19:40:24 +0600

    styles cleanup. organized.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,30 @@
 import { reactive } from 'vue'
 
 defineProps<{
-  msg: string
+   msg: string
 }>()
 
 interface YesNoResponse {
-	answer: string,
-	forced: boolean,
-	image: string
+   answer: string,
+   forced: boolean,
+   image: string
 }
 
 let yesno = reactive({} as YesNoResponse);
 
 fetch('https://yesno.wtf/api')
    .then(response => response.json())
-   .then((data:YesNoResponse) => Object.assign(yesno, data));
+   .then((data: YesNoResponse) => Object.assign(yesno, data));
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-	<div>
-		<div>answer: {{ yesno.answer }}</div>
-		<div><img v-bind:src="yesno.image || 'javascript:void(0)'" alt="yes/no gif" /></div>
-	</div>
-  </div>
+   <div>
+      <h3>{{ msg }}</h3>
+      <div>
+         <h5>Sample HTTP request-response from <i>yesno.wtf</i>:</h5>
+         <div>{{ JSON.stringify(yesno) }}</div>
+      </div>
+   </div>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,33 +3,23 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-   <v-container class="fill-height">
-      <v-responsive class="d-flex fill-height">
-         <div class="cover-image d-flex" style="justify-content:space-between;">
-            <div></div>
-            <div class="d-flex flex-column justify-center pa-2" :style="$vuetify.display.xs ? 'width:80%' : 'width:50%'">
-               <div class="d-flex align-center">
-                  <span class="mr-2">Make</span>
-                  <v-text-field hide-details :variant="$vuetify.display.xs ? 'filled' : 'outlined'" style="width:100%;" />
-               </div>
-               <div class=" d-flex align-center mt-3">
-                  <span class="mr-2">Model</span>
-                  <v-select hide-details :variant="$vuetify.display.xs ? 'filled' : 'outlined'" style="width:100%;" />
-               </div>
-               <v-btn :flat="!$vuetify.display.xs" color="primary" class="mt-3">Save</v-btn>
-            </div>
-         </div>
-         <HelloWorld msg="You did it!" />
-      </v-responsive>
-   </v-container>
+   <div>
+      <HelloWorld msg="A Vue component" />
+      <br/>
+      <br/>
+      <h3>Some Vuetify components</h3>
+      <v-container>
+            <!-- input component's "hide-details" attribute set
+               to 'true' globally while initiating Vuetify -->
+            <v-text-field :variant="$vuetify.display.xs ? 'filled' : 'outlined'" style="width:100%;" />
+            <br />
+            <v-select :variant="$vuetify.display.xs ? 'filled' : 'outlined'" style="width:100%;" />
+            <br />
+            <v-btn :flat="!$vuetify.display.xs" color="primary" class="mt-3">Save</v-btn>
+      </v-container>
+   </div>
 </template>
 
-<style>
-.cover-image {
-   background: linear-gradient(rgba(206, 205, 205, 0.4), rgba(240, 239, 239, 0.4));
-   background-position: center center;
-   background-repeat: no-repeat;
-   background-size: cover;
-   height: 250px;
-}
+<style scoped>
+
 </style>
